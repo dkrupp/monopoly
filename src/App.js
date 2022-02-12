c2d25a9c075fbab61cff0681bcf99f877ed29fc8
Gábor Klinda <gabor.klinda@gmail.com>
2022-02-12 11:59:38 +0100

    set up eslint and prettier

--- src/App.js
+++ src/App.js
@@ -2,6 +2,6 @@ import { Client } from 'boardgame.io/react';
 import { Monopoly } from './Game';
 import { MonopolyBoard } from './board/Board.js';
 
-const App = Client({ game: Monopoly, board: MonopolyBoard });
+const App = Client({ game: Monopoly, board: MonopolyBoard, numPlayers: 1 });
 
 export default App;
